baefea526c312eb1f544b758849ff1ea5f3d1e97
Yanina063 <strrrekosa@mail.ru>
2022-07-15 23:02:21 +0400

    practic1

---
+++ DZ1/hello.py
@@ -0,0 +1,24 @@
+# типы данных и переменная
+# int, float, boolean, str, list, None
+# value = None
+a = 123
+b = 1.23
+# print(type(a))
+# print(tipe(b))
+# value = 12334
+# print(type(value))
+s = 'hello world'
+
+print(f'{a} - {b} - {s}')
+f = True
+print(f)
+
+print('Введите a');
+a =int(input())
+print('Введите b');
+b = int(input())
+print(a, '+', b, '=', a+b)
+print(f'{a} {b}')
+
+a = 1 < 4 and 5 > 2 # 1 < 3 < 5
+print(a)
